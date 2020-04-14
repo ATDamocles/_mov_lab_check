b93e7c583d87096e7913fd1502c4b474bd76b2c0
ATDamocles <54849479+ATDamocles@users.noreply.github.com>
2020-04-15 00:01:51 +0300

    lab_03

---
+++ Git_lw_03.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string strInput;
+    cout << "======================== ++++++ =====================" << endl
+         << "=== This program finds '<<' and change into '>>'  ===" << endl;
+    cout << "Input a string: ";
+    getline(cin, strInput);    
+    while (strstr(strInput.c_str(), "<<"))
+    {
+        strInput.replace(strInput.find("<<"), 2, ">>");
+    }  
+    cout << strInput;
+    return 0;
+}
+
